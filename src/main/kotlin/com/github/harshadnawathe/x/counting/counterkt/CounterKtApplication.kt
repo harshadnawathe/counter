41e81fc5d3bcb43ebbcf7ee336a25f04dd496950
Harshad Nawathe <harshadn@thoughtworks.com>
2022-03-22 12:21:00 +0530

    Initial commit

---
+++ src/main/kotlin/com/github/harshadnawathe/x/counting/counterkt/CounterKtApplication.kt
@@ -0,0 +1,11 @@
+package com.github.harshadnawathe.x.counting.counterkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CounterKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<CounterKtApplication>(*args)
+}
